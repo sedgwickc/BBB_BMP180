831942348b5a5b7e396ed38dd942ce87d2d177e2
Charles Sedgwick <sedgwickcharles@gmail.com>
2016-04-05 14:34:07 +0000

    Add attributes, defines and structs to header.

--- Adafruit_BMP180.hpp
+++ Adafruit_BMP180.hpp
@@ -20,8 +20,7 @@
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 
-#include "Adafruit_BMP180.hpp"
-#include "mraa.h"
+#include "mraa.hpp"
 #include <math.h>
 #include <limits.h>
 #include <iostream>
@@ -35,6 +34,95 @@ using namespace std;
 namespace rover {
 
 #define BMP180_USE_DATASHEET_VALS (0) /* Set to 1 for sanity check */
+#define BMP180_DEBUG 0
+
+#define BMP180_I2CADDR 0x77
+
+#define BMP180_ULTRALOWPOWER 0
+#define BMP180_STANDARD      1
+#define BMP180_HIGHRES       2
+#define BMP180_ULTRAHIGHRES  3
+#define BMP180_CAL_AC1           0xAA  // R   Calibration data (16 bits)
+#define BMP180_CAL_AC2           0xAC  // R   Calibration data (16 bits)
+#define BMP180_CAL_AC3           0xAE  // R   Calibration data (16 bits)    
+#define BMP180_CAL_AC4           0xB0  // R   Calibration data (16 bits)
+#define BMP180_CAL_AC5           0xB2  // R   Calibration data (16 bits)
+#define BMP180_CAL_AC6           0xB4  // R   Calibration data (16 bits)
+#define BMP180_CAL_B1            0xB6  // R   Calibration data (16 bits)
+#define BMP180_CAL_B2            0xB8  // R   Calibration data (16 bits)
+#define BMP180_CAL_MB            0xBA  // R   Calibration data (16 bits)
+#define BMP180_CAL_MC            0xBC  // R   Calibration data (16 bits)
+#define BMP180_CAL_MD            0xBE  // R   Calibration data (16 bits)
+
+#define BMP180_CONTROL           0xF4 
+#define BMP180_TEMPDATA          0xF6
+#define BMP180_PRESSUREDATA      0xF6
+#define BMP180_READTEMPCMD          0x2E
+#define BMP180_READPRESSURECMD            0x34
+
+/*=========================================================================
+    I2C ADDRESS/BITS
+    -----------------------------------------------------------------------*/
+    #define BMP180_ADDRESS                (0x77)
+/*=========================================================================*/
+
+/*=========================================================================
+    REGISTERS
+    -----------------------------------------------------------------------*/
+    enum
+    {
+      BMP180_REGISTER_CAL_AC1            = 0xAA,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_AC2            = 0xAC,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_AC3            = 0xAE,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_AC4            = 0xB0,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_AC5            = 0xB2,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_AC6            = 0xB4,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_B1             = 0xB6,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_B2             = 0xB8,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_MB             = 0xBA,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_MC             = 0xBC,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CAL_MD             = 0xBE,  // R   Calibration data (16 bits)
+      BMP180_REGISTER_CHIPID             = 0xD0,
+      BMP180_REGISTER_VERSION            = 0xD1,
+      BMP180_REGISTER_SOFTRESET          = 0xE0,
+      BMP180_REGISTER_CONTROL            = 0xF4,
+      BMP180_REGISTER_TEMPDATA           = 0xF6,
+      BMP180_REGISTER_PRESSUREDATA       = 0xF6,
+      BMP180_REGISTER_READTEMPCMD        = 0x2E,
+      BMP180_REGISTER_READPRESSURECMD    = 0x34
+    };
+/*=========================================================================*/
+
+/*=========================================================================
+    MODE SETTINGS
+    -----------------------------------------------------------------------*/
+    typedef enum
+    {
+      BMP180_MODE_ULTRALOWPOWER          = 0,
+      BMP180_MODE_STANDARD               = 1,
+      BMP180_MODE_HIGHRES                = 2,
+      BMP180_MODE_ULTRAHIGHRES           = 3
+    } bmp180_mode_t;
+/*=========================================================================*/
+
+/*=========================================================================
+    CALIBRATION DATA
+    -----------------------------------------------------------------------*/
+    typedef struct
+    {
+      int16_t  ac1;
+      int16_t  ac2;
+      int16_t  ac3;
+      uint16_t ac4;
+      uint16_t ac5;
+      uint16_t ac6;
+      int16_t  b1;
+      int16_t  b2;
+      int16_t  mb;
+      int16_t  mc;
+      int16_t  md;
+    } bmp180_calib_data;
+/*=========================================================================*/
 
 class Adafruit_BMP180{
 
@@ -63,5 +151,9 @@ public:
 	float pressureToAltitude(float seaLevel, float atmospheric, float temp);
 	float seaLevelForAltitude(float altitude, float atmospheric);
 	float seaLevelForAltitude(float altitude, float atmospheric, float temp);
-	} //BMP180 class
-} // bmp180 namespace
+private:
+	mraa::I2c *i2c_bmp180;
+	unsigned int I2CAddress;
+	//registers;
+	}; //BMP180 class
+} // rover namespace
